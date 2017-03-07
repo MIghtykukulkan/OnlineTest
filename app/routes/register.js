8f3b7a8880948af8e919ebc4c28e91d27a23faf6
swetavahia2117 <sweta.vahia@rapidqube.com>
2017-03-07 12:23:44 +0530

    Adding questions

--- app/routes/register.js
+++ app/routes/register.js
@@ -2,8 +2,28 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-model()
+ /* model()
 {
-    
+ /* createUser: function() {
+ var fname = this.get('controller').get('fname');
+ var lname = this.get('controller').get('lname');
+ var phone = this.get('controller').get('phone');
+ var email = this.get('controller').get('email');
+
+ 
+ App.Adapter.ajax('/registerUser', {
+   type: 'POST',
+   data: {fname,lname,phone,email},
+   context: this}).then(function(data) {
+     var user = App.User.createUser(data);
+     this.modelFor('users').pushObject(user);
+     this.get('controller').set('newName', '');
+     this.transitionTo('artist.songs', artist);
+ }, function(reason) {
+   alert('Failed to save users');
+ });
 }
+}  
+*/
 });
+
