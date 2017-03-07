c18beda54a6e273b4682ec48e7fb5bae47669d1f
Shraddhak <Shraddhak@corp.rpqb.com>
2017-03-07 14:44:41 +0530

    upadte changes

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -2,21 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
    
-  actions:{
-    
-           taketest: function() {
-           this.transitionToRoute('test');
-        },
+  
+actions:
+{
+        taketest: function() {
+            this.transitionToRoute('test');
+        }
+} 
 
-        registerUser: function(user) {      
-      $.ajax({
-        type: "POST",
-        url: "http://ec2-54-218-55-72.us-west-2.compute.amazonaws.com:8080/registerUser",
-        data: { fname: 'fname', lname: 'lname',phone:'phone',email:'email' }
-      })
-      this.transitionTo('test');
-
-    }
-
-  }
 });
