90201cd69f10773fb654230a52dfe6e34b7c2ce7
swetavahia2117 <sweta.vahia@rapidqube.com>
2017-03-07 12:28:34 +0530

    updates

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -6,8 +6,17 @@ export default Ember.Controller.extend({
     
            taketest: function() {
            this.transitionToRoute('test');
-        }
+        },
 
-        
+        registerUser: function(user) {      
+      $.ajax({
+        type: "POST",
+        url: "http://ec2-54-218-55-72.us-west-2.compute.amazonaws.com:8080/registerUser",
+        data: { fname: 'fname', lname: 'lname',phone:'phone',email:'email' }
+      })
+      this.transitionTo('test');
+
+    }
+
   }
 });
