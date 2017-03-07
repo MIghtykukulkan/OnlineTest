c8d13ac372319a16ea81a02d4744c7fa401e4568
rakeshkumarb <rakesh.bharati@rapidqube.com>
2017-03-07 18:34:48 +0530

    upadate

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -3,15 +3,9 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     actions: {
         taketest: function() {
-          var firstName = this.get('fname');
-          console.log("FirstName: " + firstName);
-          if (firstName === undefined || firstName === null) {
-              this.set('errorMessage', "Please enter First Name");
-              return false;
-          } else {
-              this.set('errorMessage', "");
+          
               this.transitionToRoute('test');
           }
-        }
-    },
+        },
+    
 });
