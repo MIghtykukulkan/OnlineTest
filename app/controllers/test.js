6ea215dd30100b9db2c23162b60fefbef87509bc
rakeshkumarb <rakesh.bharati@rapidqube.com>
2017-03-09 12:22:10 +0530

    push latest code

--- app/controllers/test.js
+++ app/controllers/test.js
@@ -3,22 +3,22 @@ import Ember from 'ember';
 
 
 export default Ember.Controller.extend({
-    
-    testlist: ['Programming Fundamentals', 'JAVA','Language'],
-     actions: {
-         questionlist: function() {
-        var chosen = this.get('selectedtest');
-          //console.log(chosen);
-        if (chosen === null || chosen === undefined) {
-            this.set('errorMessage', "Please Select Test");
-            return false;
-        } else {
-              this.set('errorMessage', "");
-              this.transitionToRoute('programming');
-      
+
+    testlist: ['Programming Fundamentals', 'JAVA', 'Language'],
+    actions: {
+        questionlist: function() {
+            var chosen = this.get('selectedtest');
+
+            if (chosen === null || chosen === undefined) {
+                this.set('errorMessage', "Please Select Test");
+                return false;
+            } else {
+                this.set('errorMessage', "");
+                this.transitionToRoute('programming');
+
+            }
         }
-         }
-     }
+    }
 
 
 });
