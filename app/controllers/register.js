50c3b3698b483bbeba17adf46a5717cb3ccdbc6f
Vikramv <Vikramv@corp.rpqb.com>
2017-03-11 21:49:35 +0530

    Code formatting + extra <br> removed

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -2,26 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     actions: {
-
-
         registerUser: function() {
             var lastName = this.get('lname');
             var firstName = this.get('fname');
             var phoneNo = this.get('phone');
             var emailId = this.get('email');
             //var phoneNo = document.getElementById('phone');
-
-
-            var dataString = {
-                "fname": firstName,
-                "lname": lastName,
-                "phone": phoneNo,
-                "email": emailId
-            }
-
             // var datastring = this.get('model');
-
-
             var returnValue = true;
 
             //console.log("FirstName: " + firstName);
@@ -31,31 +18,29 @@ export default Ember.Controller.extend({
 
             //console.log("LastName: " + lastName);
             if (lastName === undefined || lastName === null) {
-
                 returnValue = false;
             }
 
             //console.log("Phone: " + phoneNo);
             if (phoneNo === undefined || phoneNo === null) {
-
                 returnValue = false;
             }
+
             if (phoneNo.length < 10 || phoneNo.length > 10) {
-
                 returnValue = false;
             }
+
             //console.log("email: " + emailId);
             if (emailId === undefined || emailId === null) {
-
                 returnValue = false;
             }
-            if (email.value.indexOf("@", 0) < 0) {
 
+            if (email.value.indexOf("@", 0) < 0) {
                 email.focus();
                 returnValue = false;
             }
-            if (email.value.indexOf(".", 0) < 0) {
 
+            if (email.value.indexOf(".", 0) < 0) {
                 email.focus();
                 returnValue = false;
             }
@@ -68,6 +53,12 @@ export default Ember.Controller.extend({
                 this.set('errorPHMessage', "");
                 this.set('errorEIMessage', "");
 
+                var dataString = {
+                    "fname": firstName,
+                    "lname": lastName,
+                    "phone": phoneNo,
+                    "email": emailId
+                }
                 console.log(JSON.stringify(dataString));
 
                 $.ajax({
@@ -78,16 +69,11 @@ export default Ember.Controller.extend({
                     dataType: "json",
                     success: function(response) {
                         this.transitionToRoute('test');
-                        alert(response)
                     },
                     failure: function(result) {
                         alert(result)
                     }
                 })
-
-
-
-
             } else {
                 this.set('errorFNMessage', "Please enter First Name");
                 this.set('errorPNMessage', "Please enter Phone Number");
@@ -96,7 +82,6 @@ export default Ember.Controller.extend({
                 this.set('errorPHMessage', "Please enter 10 digit no.");
                 this.set('errorEIMessage', "Please enter coorect Email Id");
             }
-
         }
     },
 });
