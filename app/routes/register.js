70b43eddfcb200244c0f2cde28e3fae7e71aaf85
swetavahia2117 <sweta.vahia@rapidqube.com>
2017-03-14 16:25:04 +0530

    massage

--- app/routes/register.js
+++ app/routes/register.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
+import CONFIG from 'online-test/config/environment';
 
 export default Ember.Route.extend({
     model() {
+        console.log(CONFIG);
         this.controllerFor('register').set('loading_image_visibility', 'hide');
         this.controllerFor('register').set('firstname', null);
         this.controllerFor('register').set('lastname', null);
