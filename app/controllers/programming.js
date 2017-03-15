6022a8a7f334a2c0814ef24afda828234902d8b6
Shraddhak <Shraddhak@corp.rpqb.com>
2017-03-15 14:56:42 +0530

    pull

--- app/controllers/programming.js
+++ app/controllers/programming.js
@@ -33,9 +33,9 @@ export default Ember.Controller.extend({
             for(var i=0; i< model.length; i++){
 
           var dataStringsc ={
-                    "uid": model.message,
+                    "uid": message,
                     "q_type": model[i].id,
-                    "score": model[i].userAnswer
+                    "selected": model[i].userAnswer
                      
                 }
         
@@ -56,8 +56,10 @@ export default Ember.Controller.extend({
                     },
                     error: function(result) {
                          console.log(result)
-                    }
+                    },
+                    
                 })
+                
 
         },
         toggleModalOk:function(){
