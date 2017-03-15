4e590364b342df576dcdefaf2a565580b7e4dcd8
rakeshkumarb <rakesh.bharati@rapidqube.com>
2017-03-15 18:32:18 +0530

    pull

--- app/controllers/report.js
+++ app/controllers/report.js
@@ -1,28 +1,55 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-   
+  
 columns: [
   {
     "propertyName": "uid",
-    "title": "uid"
+    "title": "Uid"
   },
   {
     "propertyName": "fname",
-    "title": "fname"
+    "title": "Fname"
   },
   {
     "propertyName": "java",
-    "title": "java"
+    "title": "Java"
   },
   {
     "propertyName": "fundamental",
-    "title": "fundamental"
+    "title": "Fundamental"
   },
   {
     "propertyName": "language",
-    "title": "language"
+    "title": "Language"
   },
   
 ],
+ 
+/*actions: {
+
+  reportCard : function() {
+    var data;
+   return $.ajax({
+                    url: CONFIG.GOURL+'/report',
+                    type: 'GET',
+                    accepts: 'application/json',
+                    success: function(data) {
+                        //alert("success"+JSON.stringify(data))
+                        console.log(JSON.stringify(data))                     
+                        return data,
+                        
+                        console.log('DEBUG: GET Enquiries OK');
+                    },
+                    error: function(err) {
+                        console.log(data)
+                        alert(err)
+                        console.log('DEBUG: GET Enquiries Failed');
+                    }
+                });
+               
+            }
+            }); */
+
+}
 });
