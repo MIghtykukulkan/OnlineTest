7aa33c74479aa561a6f04175c34b12a2cc329f98
rakeshkumarb <rakesh.bharati@rapidqube.com>
2017-03-15 17:42:51 +0530

    Qbackgroundcolor

--- app/controllers/addquestion.js
+++ app/controllers/addquestion.js
@@ -2,25 +2,26 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     testlist: ['Fundamental', 'JAVA', 'Language'],
+    
      actions: {
           questionlist : function() {
 
             var question = this.get('question');
-            var option1 = this.get('option1');
-            var option2 = this.get('option2');
-            var option3 = this.get('option3');
-            var option4 = this.get('option4');
-            var answer = this.get('answer');
+            var option1  = this.get('option1');
+            var option2  = this.get('option2');
+            var option3  = this.get('option3');
+            var option4  = this.get('option4');
+            var answer   = this.get('answer');
             //var phoneNo = document.getElementById('phone');
 
 
             var dataString = {
                 "question": question,
-                "option1": option1,
-                "option2": option2,
-                "option3": option3,
-                "option4": option4,
-                "answer": answer,
+                "option1" : option1,
+                "option2" : option2,
+                "option3" : option3,
+                "option4" : option4,
+                "answer"  : answer,
             }
             console.log(JSON.stringify(dataString));
             $.ajax({
