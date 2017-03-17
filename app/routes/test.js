789f1fb4db8e7434e707c64b9889da9782a51bda
Shraddhak <Shraddhak@corp.rpqb.com>
2017-03-17 19:09:49 +0530

    update

--- app/routes/test.js
+++ app/routes/test.js
@@ -2,15 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-   
-
-
     model: function() {
           var message = this.controllerFor('home').get('token');
            console.log('message');
+           this.toggleProperty('isShowingModal');
             this.controllerFor('test').set('token', message); 
-             this.controllerFor('test').set('loading_image_visibility', 'hide');
-    }
+             this.controllerFor('test').set('loading_image_visibility', 'show');
+    },
     /* client does not have a valid session token */
     /* client has a persisted token */
       // use persisted token to get a valid session token, return a promise
@@ -19,6 +17,9 @@ export default Ember.Route.extend({
       // client does not have any valid tokens
       // abort transition, redirect to login page
     
-  
+afterModel : function() {
+  this.toggleProperty('isShowingModal');
+ this.controllerFor('test').set('loading_image_visibility', 'hide');
+}
 
 });
