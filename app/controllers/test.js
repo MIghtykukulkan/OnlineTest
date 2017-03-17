d4ee8d84a5951df37923e6c5b1538695df49cfb1
swetavahia2117 <sweta.vahia@rapidqube.com>
2017-03-17 16:13:43 +0530

    pull

--- app/controllers/test.js
+++ app/controllers/test.js
@@ -3,6 +3,10 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     testlist: ['Fundamental', 'JAVA', 'Language',],
     actions: {
+         log_out1 : function(){
+            this.transitionToRoute('home');
+         },
+
         questionlist: function() {
             var chosen = this.get('selectedtest');
         console.log(chosen);
